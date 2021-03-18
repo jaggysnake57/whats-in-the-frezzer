9ed3bd19d0e1088d5c5ea232e965db2e4bcf24dd
Dean Richards <jaggysnake57@sky.com>
2021-03-18 10:53:26 +0000

    add basic flash message functionality

--- src/App.js
+++ src/App.js
@@ -29,16 +29,56 @@ import './App.css';
 import User from './Pages/User/User';
 import UserEdit from './Pages/UserEdit/UserEdit';
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import FlashMessage from './Components/FlashMessage/FlashMessage';
+import { clearMessage, selectUI, setMessage } from './features/UI/UISlice';
 
 function App() {
 	const [items, setItems] = useState({});
 	const dispatch = useDispatch();
 	const { username, userDocId } = useSelector(selectUser);
-	const { message, error } = useSelector(selectItems);
+	const { message } = useSelector(selectUI);
 	const history = useHistory();
 
-	const handleUser = () => {
-		dispatch(getUser());
+	history.listen((location, action) => {
+		dispatch(clearMessage());
+	});
+
+	const handleTestFlash = (type) => {
+		switch (type) {
+			case 'error':
+				dispatch(
+					setMessage({
+						type: 'error',
+						content: 'this is a test error message',
+					})
+				);
+				break;
+			case 'success':
+				dispatch(
+					setMessage({
+						type: 'success',
+						content: 'this is a test success message',
+					})
+				);
+				break;
+			case 'general':
+				dispatch(
+					setMessage({
+						type: 'general',
+						content: 'this is a test general message',
+					})
+				);
+				break;
+
+			default:
+				dispatch(
+					setMessage({
+						type: 'error',
+						content: 'something actualy went wrong',
+					})
+				);
+				break;
+		}
 	};
 
 	useEffect(() => {
@@ -52,11 +92,19 @@ function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<div className="message">
-				{message}
-				{error?.message}
-			</div>
+			{message.type && <FlashMessage />}
 			<div className="container">
+				<div style={{ marginTop: '5rem' }}>
+					<button onClick={() => handleTestFlash('error')}>
+						warn
+					</button>
+					<button onClick={() => handleTestFlash('success')}>
+						success
+					</button>
+					<button onClick={() => handleTestFlash('general')}>
+						general
+					</button>
+				</div>
 				{!username ? <Redirect to="/login" /> : null}
 				<Switch>
 					<PrivateRoute exact path="/" component={Home} />
